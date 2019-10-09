826b09eed8cf6831a087543d5f803cb69ebf08b9
cyd3r <cyd3rhacker@gmail.com>
2019-10-09 21:12:41 +0200

    auto detect the arduino

--- manage.py
+++ manage.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # use this script to manage the passwords on the device
 
-# TODO: automatically detect the correct port (cross-platform)
-
 import sys
 from getpass import getpass
 from serial import Serial
+from serial.tools.list_ports import comports
 
 def usage():
     print("Usage:")
@@ -13,6 +12,12 @@ def usage():
     print(" ", sys.argv[0], "add")
     print(" ", sys.argv[0], "reset")
     exit(1)
+
+def get_port():
+    for port in comports():
+        if "VID:PID=2341:0037" in port.usb_info():
+            return port[0]
+    raise Exception("Auto detection failed")
 
 if __name__ == "__main__":
     try:
@@ -23,17 +28,17 @@ if __name__ == "__main__":
             name = input("Account: ")
             password = getpass("Password: ")
             msg = name + "\x1F" + password + "\n"
-            with Serial("/dev/ttyACM0", 9600) as ser:
+            with Serial(get_port(), 9600) as ser:
                 ser.write(msg.encode("ascii"))
 
         elif sys.argv[1] == "rm":
             name = input("Account: ")
             msg = "\x08" + name + "\n"
-            with Serial("/dev/ttyACM0", 9600) as ser:
+            with Serial(get_port(), 9600) as ser:
                 ser.write(msg.encode("ascii"))
 
         elif sys.argv[1] == "reset":
-            with Serial("/dev/ttyACM0", 9600) as ser:
+            with Serial(get_port(), 9600) as ser:
                 ser.write("\x7F\n".encode("ascii"))
 
         else:
